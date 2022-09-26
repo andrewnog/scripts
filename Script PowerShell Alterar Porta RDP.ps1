3187419f52d128522a9dfacfb5cfd7f24ff5dfba
Andrew Pereira Nogueira <101531900+andrewnog@users.noreply.github.com>
2022-09-26 20:54:30 -0300

    Add files via upload
    Alterar porta RDP

---
+++ Script PowerShell Alterar Porta RDP.ps1
@@ -0,0 +1,17 @@
+﻿### Script para alterar a porta do protocolo RDP para máquinas Windows ###
+### No PowerShell, execute: (Onde 3390 é o numero da porta escolhida.) ###
+
+
+$portvalue = 3390
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $portvalue 
+
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
+
+
+
+### Para consultar a alteração, execute o comando:
+
+
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber"
