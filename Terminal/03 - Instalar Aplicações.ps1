ecb9123ce5348632538d57528ff623e6c3dbbc85
Andrew Pereira Nogueira <101531900+andrewnog@users.noreply.github.com>
2024-02-01 16:16:17 -0300

    Delete Terminal/03 - Instalar Aplicações.ps1

--- Terminal/03 - Instalar Aplicações.ps1
+++
@@ -1,8 +0,0 @@
-﻿winget install Google.Chrome 
-# winget install Google.ChromeRemoteDesktop 
-winget install RARLab.WinRAR 
-winget install AnyDeskSoftwareGmbH.AnyDesk 
-winget install AcroSoftware.CutePDFWriter 
-winget install Oracle.JavaRuntimeEnvironment 
-winget install Adobe.Acrobat.Reader.64-bit
-winget install Famatech.AdvancedIPScanner
