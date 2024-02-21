84924dd650919fcff165f2ab84e252f4c5c36bb0
Andrew Pereira Nogueira <101531900+andrewnog@users.noreply.github.com>
2024-02-21 12:36:47 -0300

    Add files via upload

---
+++ Servidor/01 - Instalar Windows Admin Center.ps1
@@ -0,0 +1,8 @@
+﻿# Navegar até a pasta Downloads
+cd C:\Users\Public\Downloads
+
+# Fazer o download do Windows Admin Center
+wget https://aka.ms/WACDownload -UseBasicParsing -OutFile WAC.msi
+
+# Iniciar o instalador do pacote do Windows Admin Center
+.\WAC.msi
