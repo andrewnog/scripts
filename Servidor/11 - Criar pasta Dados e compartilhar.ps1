78e221c357bde880070e51e91e4c12b486512983
Andrew Pereira Nogueira <101531900+andrewnog@users.noreply.github.com>
2024-10-03 17:23:36 -0300

    Add files via upload

---
+++ Servidor/11 - Criar pasta Dados e compartilhar.ps1
@@ -0,0 +1,4 @@
+﻿mkdir C:\Dados
+New-SmbShare -Name "Dados" -Path "C:\Dados" -FullAccess "Everyone"
+cd C:\Dados
+mkdir "Administrativo", "Depto Contabil", "Depto Legal", "Depto Pessoal", "Depto Fiscal", "Diretoria", "Financeiro", "Publico", "Transferencia"
