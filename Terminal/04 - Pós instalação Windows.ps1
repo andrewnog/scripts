31404d4458dfd7c1fc21bd8875a865e01b958d60
Andrew Pereira Nogueira <101531900+andrewnog@users.noreply.github.com>
2024-10-30 01:47:41 -0300

    remove cutepdf

--- Terminal/04 - Pós instalação Windows.ps1
+++ Terminal/04 - Pós instalação Windows.ps1
@@ -9,7 +9,7 @@ winget install Google.Chrome
 # winget install Google.ChromeRemoteDesktop 
 winget install RARLab.WinRAR 
 winget install AnyDeskSoftwareGmbH.AnyDesk 
-winget install AcroSoftware.CutePDFWriter 
+# winget install AcroSoftware.CutePDFWriter 
 winget install Oracle.JavaRuntimeEnvironment 
 winget install Adobe.Acrobat.Reader.64-bit
 winget install OpenVPNTechnologies.OpenVPNConnect
